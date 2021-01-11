30d860cbcd7713b69cd037a1c81f2b8d16ad28f8
fresh-cut <dorofeevvadim86@mail.com>
2021-01-11 19:56:49 +0300

    finish

--- config/captcha.php
+++ config/captcha.php
@@ -1,16 +1,5 @@
 <?php
 
-//$setting=app(App\Settings::class);
-//
-//return [
-//    'secret' => settings('google_recapcha_secret_key',''),
-//    'sitekey' => settings('google_recapcha_site_key', ''),
-//    'options' => [
-//        'timeout' => 30,
-//    ],
-//];
-
-
 return [
     'secret' => env('NOCAPTCHA_SECRET'),
     'sitekey' => env('NOCAPTCHA_SITEKEY'),
